6d88c89e089fe50027e4c566c93043e3b92f55c6
João Goulart <joao123goulart@gmail.com>
2023-03-17 10:36:12 -0300

    17/03/2023 --> Menu inserção de dados ok

--- T2/app.py
+++ T2/app.py
@@ -1,6 +1,7 @@
 """
     --------------------------------------------------------
-    Engenharia de dados - Trabalho 2
+	    	  Engenharia de dados - Trabalho 2
+    --------------------------------------------------------
     Implementação de um banco de dados multidimensional.
     --------------------------------------------------------
     Made with: Python, MongoDB e mongoengine
@@ -8,27 +9,18 @@
     --------------------------------------------------------
 """
 
-from mongoengine import *
-from lib import entrada_dados, insere_dados
+from lib import *
 
 DB_HOST = "mongodb+srv://mongodb:mongodb@eng-dados-1.dawrqwk.mongodb.net/?retryWrites=true&w=majority"
 
 def main():
-    try:
-        # Definindo cliente mongo e instanciando banco
-        # client = pymongo.MongoClient(DB_HOST)
-        # db = client.get_database(name="eng-dados-1")
-        # Conectando mongoengine
-        connect(host=DB_HOST)
-        print("Conectado ao banco!")
-    except Exception as e:
-        print(e)
+    conexao_db()
+    # Exibe menu de interação
+    while True:
+        try:
+            dados, funcionou = menu()
 
-    # Ler dados a partir do teclado
-    entrada = entrada_dados()
-
-    # Insere entrada no banco
-    insere_dados(entrada)
-
+        except Exception as e:
+            print(e)
 
 main()
